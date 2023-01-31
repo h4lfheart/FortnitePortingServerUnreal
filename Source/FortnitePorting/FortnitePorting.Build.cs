be87afae4dcfbce5b157981c50eca8577f35e9cf
GMatrixGames <gmatrixgames@hotmail.com>
2023-01-31 00:34:26 -0500

    built-in json

--- Source/FortnitePorting/FortnitePorting.Build.cs
+++ Source/FortnitePorting/FortnitePorting.Build.cs
@@ -6,31 +6,31 @@ public class FortnitePorting : ModuleRules
 {
 	public FortnitePorting(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
 		PublicIncludePaths.AddRange(
-			new string[] {
+			new string[]
+			{
 				// ... add public include paths required here ...
 			}
-			);
-				
+		);
 		
 		PrivateIncludePaths.AddRange(
-			new string[] {
+			new string[]
+			{
 				// ... add other private include paths required here ...
 			}
-			);
-			
+		);
 		
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"Core", "Networking"
+				"Core", "Networking", "Json", "JsonUtilities"
 				// ... add other public dependencies that you statically link with here ...
 			}
-			);
-			
-		
+		);
+
+
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
@@ -41,14 +41,13 @@ public class FortnitePorting : ModuleRules
 				"Sockets",
 				"Networking"
 			}
-			);
-		
-		
+		);
+
 		DynamicallyLoadedModuleNames.AddRange(
 			new string[]
 			{
 				// ... add any modules that your module loads dynamically here ...
 			}
-			);
+		);
 	}
 }
